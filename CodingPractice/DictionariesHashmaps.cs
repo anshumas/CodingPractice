5585038bcaba2127e61b94f65622e3756c9fd283
Anshuman Singh <anshumas@gmail.com>
2018-10-03 01:19:12 -0500

    added link for problem

--- CodingPractice/DictionariesHashmaps.cs
+++ CodingPractice/DictionariesHashmaps.cs
@@ -86,6 +86,9 @@ namespace CodingPractice
         }
         #endregion
         #region Frequency Queries
+        /// <summary>
+        /// https://www.hackerrank.com/challenges/frequency-queries/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+        /// </summary>
         [TestMethod]
         public void freqQueryTest()
         {
