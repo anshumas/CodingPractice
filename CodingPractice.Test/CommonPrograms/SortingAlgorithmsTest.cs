32817c5a1f17987ab185ac53c91846aabce2c832
Anshuman Singh <anshumas@gmail.com>
2019-06-10 23:49:15 -0500

    sorting

--- CodingPractice.Test/CommonPrograms/SortingAlgorithmsTest.cs
+++ CodingPractice.Test/CommonPrograms/SortingAlgorithmsTest.cs
@@ -21,17 +21,40 @@ namespace CodingPractice.Test.CommonPrograms
             List<int> unsorted = new List<int>();
             List<int> sorted;
             Random random = new Random();
-            
+
             for (int i = 0; i < 2000000; i++)
             {
-                unsorted.Add(random.Next(1, 100000));
+                unsorted.Add(random.Next(1, 2000000));
             }
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            sorted =_object.MergeSort(unsorted);
+            sorted = _object.MergeSort(unsorted);
             stopwatch.Stop();
-           string totaltime= stopwatch.Elapsed.ToString();
-            Assert.AreEqual(sorted.Count, 10);
+            string totaltime = stopwatch.Elapsed.ToString();
+            Assert.AreEqual(totaltime, 10);
+            /*
+             20,000  -00:00:00.1306896
+             2,00,000-00:00:02.6504209
+             20,00,000-
+             */
+        }
+        [TestMethod]
+        public void MergeSortArrayTest()
+        {
+            int[] unsorted = new int[2000000];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 2000000; i++)
+            {
+                unsorted[i] = (random.Next(1, 2000000));
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[] result = _object.MergeSort(unsorted);
+            stopwatch.Stop();
+            string totaltime = stopwatch.Elapsed.ToString();
+            Assert.AreEqual(totaltime, 10);
             /*
              20,000  -00:00:00.1306896
              2,00,000-00:00:02.6504209
@@ -45,20 +68,89 @@ namespace CodingPractice.Test.CommonPrograms
             List<int> sorted;
             Random random = new Random();
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 2000000; i++)
             {
-                unsorted.Add(random.Next(1, 20));
+                unsorted.Add(random.Next(1, 2000000));
             }
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             sorted = _object.QuickSort(unsorted);
             stopwatch.Stop();
             string totaltime = stopwatch.Elapsed.ToString();
-            Assert.AreEqual(sorted.Count, 10);
+            Assert.AreEqual(sorted.Count, unsorted.Count);
             /*
              20,000  -00:00:00.0076616
              2,00,000-00:00:00.0455105
-             20,00,000-00:00:00.9286194
+             20,00,000-00:00:00.9556085
+             */
+        }
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            int[] unsorted = new int[20000];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 20000; i++)
+            {
+                unsorted[i] = (random.Next(1, 200));
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[] result = _object.BubbleSort(unsorted);
+            stopwatch.Stop();
+            string totaltime = stopwatch.Elapsed.ToString();
+            Assert.AreEqual(totaltime, 10);
+            /*
+             20,000  -00:00:00.1306896
+             2,00,000-00:00:02.6504209
+             20,00,000-
+             */
+        }
+        [TestMethod]
+        public void SelectionSortTest()
+        {
+            int[] unsorted = new int[20000];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 20000; i++)
+            {
+                unsorted[i] = (random.Next(1, 200));
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[] result = _object.SelectionSort(unsorted);
+            stopwatch.Stop();
+            string totaltime = stopwatch.Elapsed.ToString();
+            Assert.AreEqual(totaltime, 10);
+            /*
+             20,000  -00:00:00.1306896
+             2,00,000-00:00:02.6504209
+             20,00,000-
+             */
+        }
+        [TestMethod]
+        public void HeapSortTest()
+        {
+            int[] unsorted = new int[2000000];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 2000000; i++)
+            {
+                unsorted[i] = (random.Next(1, 2000000));
+            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[] result = _object.HeapSort(unsorted);
+            stopwatch.Stop();
+            string totaltime = stopwatch.Elapsed.ToString();
+            Assert.AreEqual(totaltime, 10);
+            /*
+             20,000  -00:00:00.0192972
+             2,00,000-00:00:00.2128037
+             20,00,000-02.1107629
              */
         }
     }
--- CodingPractice/SortingAlgorithms.cs
+++ CodingPractice/SortingAlgorithms.cs
@@ -16,8 +16,14 @@ namespace CommonPrograms
             List<int> left = new List<int>();
             List<int> right = new List<int>();
             int middle = input.Count / 2;
-            left.AddRange(input.Take(middle));
-            right.AddRange(input.Skip(middle));
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(input[i]);
+            }
+            for (int i = middle; i < input.Count; i++)
+            {
+                right.Add(input[i]);
+            }
             left = MergeSort(left);
             right = MergeSort(right);
             return Merge(left, right);
@@ -56,6 +62,63 @@ namespace CommonPrograms
             }
             return result;
         }
+
+        public int[] MergeSort(int[] input)
+        {
+            if (input.Length <= 1)
+                return input;
+            int middle = input.Length / 2;
+            int[] left = new int[middle];
+            int[] right = new int[input.Length - middle];
+
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left[i] = input[i];
+            }
+            for (int i = 0; i < middle + input.Length % 2; i++)
+            {
+                right[i] = input[middle + i];
+            }
+            left = MergeSort(left);
+            right = MergeSort(right);
+            return Merge(left, right);
+        }
+        private int[] Merge(int[] left, int[] right)
+        {
+            List<int> result = new List<int>();
+            int leftIndex, rightIndex;
+            leftIndex = rightIndex = 0;
+            while (leftIndex < left.Length || rightIndex < right.Length)
+            {
+                if (leftIndex < left.Length && rightIndex < right.Length)
+                {
+                    if (left[leftIndex] <= right[rightIndex])
+                    {
+                        result.Add(left[leftIndex]);
+                        leftIndex++;
+                    }
+                    else if (left[leftIndex] > right[rightIndex])
+                    {
+                        result.Add(right[rightIndex]);
+                        rightIndex++;
+                    }
+                }
+                else
+                {
+                    if (leftIndex < left.Length)
+                    {
+                        result.Add(left[leftIndex]);
+                        leftIndex++;
+                    }
+                    if (rightIndex < right.Length)
+                    {
+                        result.Add(right[rightIndex]);
+                        rightIndex++;
+                    }
+                }
+            }
+            return result.ToArray();
+        }
         #endregion
         #region quick sort
         public List<int> QuickSort(List<int> inputList)
@@ -67,15 +130,15 @@ namespace CommonPrograms
         {
             if (left >= right)
                 return;
-            int pivot = inputList[(right - left) / 2];
+            int pivot = inputList[(right + left) / 2];
             int index = partition(inputList, left, right, pivot);
             if (index > 1)
             {
                 QuickSort(inputList, left, index - 1);
             }
-            if (index + 1 < right)
+            if (index < right)
             {
-                QuickSort(inputList, index + 1, right);
+                QuickSort(inputList, index, right);
             }
         }
         private int partition(List<int> inputlist, int left, int right, int pivot)
@@ -114,5 +177,94 @@ namespace CommonPrograms
             }
         }
         #endregion
+        #region bubble sort
+        public int[] BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap temp and arr[i] 
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+            return arr;
+        }
+        #endregion
+        #region Selection Sort
+        public int[] SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // One by one move boundary of unsorted subarray 
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array 
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first 
+                // element 
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+        #endregion
+        #region Heap Sort
+        public int[] HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                heapify(arr, n, i);
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                // Move current root to end 
+                swap(arr, 0, i);
+                // call max heapify on the reduced heap 
+                heapify(arr, i, 0);
+            }
+
+            return arr;
+        }
+        private void heapify(int[] arr, int n, int i)
+        {
+            int largest = i; // Initialize largest as root 
+            int left = 2 * i + 1; // left = 2*i + 1 
+            int right = 2 * i + 2; // right = 2*i + 2 
+            // If left child is larger than root 
+            if (left < n && arr[left] > arr[largest])
+                largest = left;
+
+            // If right child is larger than largest so far 
+            if (right < n && arr[right] > arr[largest])
+                largest = right;
+
+            // If largest is not root 
+            if (largest != i)
+            {
+
+                swap(arr, i, largest);
+                // Recursively heapify the affected sub-tree 
+                heapify(arr, n, largest);
+            }
+        }
+        private void swap(int[] inputlist, int left, int right)
+        {
+            if (inputlist[left] != inputlist[right])
+            {
+                int tmp = inputlist[left];
+                inputlist[left] = inputlist[right];
+                inputlist[right] = tmp;
+            }
+            #endregion
+        }
     }
 }
